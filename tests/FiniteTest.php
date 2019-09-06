b50c4bbd8e5f1a3bd2dc91338c962390aa26bc60
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-09-06 08:20:18 +0200

    Refactored pool tests

--- tests/FiniteTest.php
+++ tests/FiniteTest.php
@@ -3,20 +3,28 @@
 namespace WyriHaximus\React\Tests\Parallel;
 
 use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
 use WyriHaximus\PoolInfo\PoolInfoInterface;
 use WyriHaximus\PoolInfo\PoolInfoTestTrait;
 use WyriHaximus\React\Parallel\Finite;
+use WyriHaximus\React\Parallel\infinite;
+use WyriHaximus\React\Parallel\PoolInterface;
 use WyriHaximus\TestUtilities\TestCase;
 
 /**
  * @internal
  */
-final class FiniteTest extends TestCase
+final class FiniteTest extends AbstractPoolTest
 {
     use PoolInfoTestTrait;
 
     protected function poolFactory(): PoolInfoInterface
     {
         return new Finite(Factory::create(), 5);
+    }
+
+    protected function createPool(LoopInterface $loop): PoolInterface
+    {
+        return new Infinite($loop, 5);
     }
 }
