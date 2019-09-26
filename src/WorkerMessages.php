1756efaa7ad05af4aca72736ddfdb82c9539e7ea
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-09-26 22:17:04 +0200

    Streaming/communicating WIP

--- src/WorkerMessages.php
+++ src/WorkerMessages.php
@@ -2,20 +2,6 @@
 
 namespace WyriHaximus\React\Parallel;
 
-use parallel\Runtime;
-use parallel\Channel;
-use parallel\Future;
-use parallel\Events;
-use parallel\Events\Event;
-use parallel\Events\Input;
-use parallel\Events\Timeout;
-use parallel\Channel\Closed;
-use React\EventLoop\LoopInterface;
-use React\Promise\Deferred;
-use React\Promise\PromiseInterface;
-use Throwable;
-use function WyriHaximus\React\futurePromise;
-
 final class WorkerMessages
 {
     public const INFO = 'info';
