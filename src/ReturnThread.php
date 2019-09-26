bc9dc31f7820f95efc7e2db050f2381087a882ed
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-09-26 08:26:28 +0200

    Return thread missing doc block

--- src/ReturnThread.php
+++ src/ReturnThread.php
@@ -4,6 +4,9 @@ namespace WyriHaximus\React\Parallel;
 
 final class ReturnThread implements ThreadInterface
 {
+    /**
+     * {@inheritDoc}
+     */
     public function execute(string $target, array $args = [])
     {
         return [
